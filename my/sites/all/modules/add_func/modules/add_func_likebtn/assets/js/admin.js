fbd290a8f1df4dc9b8ee18e61d91c543a67aed40
anima4web <anima-3@ya.ru>
2015-02-04 17:59:00 +0200

    upd

--- my/sites/all/modules/add_func/modules/add_func_likebtn/assets/js/admin.js
+++ my/sites/all/modules/add_func/modules/add_func_likebtn/assets/js/admin.js
@@ -1,42 +1,40 @@
 /**
  * Test synchronization.
  */
-function testSync(ajaxurl)
-{
-    jQuery(".likebtn_test_sync_container:first img").show();
-    jQuery(".likebtn_test_sync_container:first .likebtn_test_sync_message").hide();
+function testSync( ajaxurl ) {
+	jQuery( ".add_func_likebtn_test_sync_container:first img" ).show();
+	jQuery( ".add_func_likebtn_test_sync_container:first .add_func_likebtn_test_sync_message" ).hide();
 
-    jQuery.ajax({
-        type: 'POST',
-        dataType: "json",
-        url: ajaxurl,
-        data: {
-            likebtn_account_email: jQuery("#edit-likebtn-account-data-email").val(),
-            likebtn_account_api_key: jQuery("#edit-likebtn-account-data-api-key").val()
-        },
-        success: function(response) {
-            var result_text = '';
-            if (typeof(response.result_text) != "undefined") {
-                result_text = response.result_text;
-            }
-            jQuery(".likebtn_test_sync_message:first").text(result_text).show();
-            if (typeof(response.result) == "undefined" || response.result != "success") {
-                jQuery(".likebtn_test_sync_message").css('color', 'red');
-                if (typeof(response.message) != "undefined") {
-                    var text = jQuery(".likebtn_test_sync_message").text() + ': ' + response.message;
-                    jQuery(".likebtn_test_sync_message").text(text);
-                }
-            } else {
-                jQuery(".likebtn_test_sync_message").css('color', 'green');
-            }
-            jQuery(".likebtn_test_sync_container:first img").hide();
+	jQuery.ajax( {
+		type: 'POST',
+		dataType: "json",
+		url: ajaxurl,
+		data: {
+			add_func_likebtn_account_email: jQuery( "#edit-add-func-likebtn-account-data-email" ).val(),
+			add_func_likebtn_account_api_key: jQuery( "#edit-add-func-likebtn-account-data-api-key" ).val()
+		},
+		success: function( response ) {
+			var result_text = '';
+			if ( typeof( response.result_text ) != "undefined" ) {
+				result_text = response.result_text;
+			}
+			jQuery( ".add_func_likebtn_test_sync_message:first" ).text( result_text ).show();
+			if ( typeof( response.result ) == "undefined" || response.result != "success" ) {
+				jQuery( ".add_func_likebtn_test_sync_message" ).css( 'color', 'red' );
+				if ( typeof( response.message ) != "undefined" ) {
+					var text = jQuery( ".add_func_likebtn_test_sync_message" ).text() + ': ' + response.message;
+					jQuery( ".add_func_likebtn_test_sync_message" ).text( text );
+				}
+			} else {
+				jQuery( ".add_func_likebtn_test_sync_message" ).css( 'color', 'green' );
+			}
+			jQuery( ".add_func_likebtn_test_sync_container:first img" ).hide();
+		},
+		error: function( response ) {
+			jQuery( ".add_func_likebtn_test_sync_message" ).text( 'Error' ).css( 'color', 'red' ).show();
+			jQuery( ".add_func_likebtn_test_sync_container:first img" ).hide();
+		}
+	} );
 
-        },
-        error: function(response) {
-            jQuery(".likebtn_test_sync_message").text('Error').css('color', 'red').show();
-            jQuery(".likebtn_test_sync_container:first img").hide();
-        }
-    });
-
-    return false;
+	return false;
 }
